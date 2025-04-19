2d5bacd60eac4b7b28ba37ae7570a7f17cfed241
Sugiarto Louise Danielle <u3645973@connect.hku.hk>
2025-04-19 17:37:46 +0800

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -162,7 +162,13 @@ int main() {
     
     Game game;
 
-    BattleBox.draw();
+    // Get terminal dimensions
+    int maxY, maxX;
+    getmaxyx(stdscr, maxY, maxX);
+
+    // Create battle box and heart
+    BattleBox battleBox(maxX/2 - 20, maxY/2 - 8, 40, 16);
+    Heart heart(maxX/2, maxY/2);
     
     while (true) {
         int ch = getch();
